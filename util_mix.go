eef4d80db7134d50a52515296a4b368e8078e686
julianto <julianto@lumoshive.com>
2025-05-09 23:40:23 +0700

    update uuid

--- util_mix.go
+++ util_mix.go
@@ -5,7 +5,18 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/google/uuid"
+	"github.com/lithammer/shortuuid/v4"
 )
+
+func ShortUUID() string {
+	return shortuuid.New()
+}
+
+func NewUUID() string {
+	return uuid.New().String()
+}
 
 func EnvArray(envName string) []string {
 	val := os.Getenv(envName)
